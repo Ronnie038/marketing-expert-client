7a749bbc9da552f9a50192166bb3a9581587c47f
Israfil <mdronnieronniers@gmial.com>
2022-11-07 20:17:08 +0600

    context setup done

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import Authprovider from './context/authProvider/Authprovider';
+import Authprovider from './context/authProvider/Authprovider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		{/* <Authprovider> */}
-		<App />
-		{/* </Authprovider> */}
+		<Authprovider>
+			<App />
+		</Authprovider>
 	</React.StrictMode>
 );
 
