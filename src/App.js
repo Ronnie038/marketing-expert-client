85dcbb524360994f2db86da6e9e8fa3cfc15e700
Israfil <mdronnieronniers@gmial.com>
2022-11-07 23:41:08 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './Layout/Main';
+import Home from './pages/Home/Home';
 
 function App() {
-	return <div className='App'>hello world</div>;
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <Main />,
+			children: [
+				{
+					index: true,
+					element: <Home />,
+				},
+			],
+		},
+	]);
+	return (
+		<div className='App'>
+			<RouterProvider router={router}></RouterProvider>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Layout/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+// import Footer from '../../../Footer';
+import Header from '../pages/Shared/Header';
+
+const Main = () => {
+	return (
+		<div
+			data-theme='light'
+			className='flex justify-between flex-col min-h-screen'
+		>
+			<div className=''>
+				<Header />
+				<Outlet />
+			</div>
+			{/* <Footer /> */}
+		</div>
+	);
+};
+
+export default Main;
